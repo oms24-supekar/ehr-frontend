97e916f45a96d235d05130e1238867d2786333e0
oms24-supekar <omsupekar24@gmail.com>
2026-01-30 20:09:26 +0530

    Add API_URL and loadPatients function

--- ehr-app.js
+++ ehr-app.js
@@ -1,3 +1,10 @@
+const API_URL = "https://ehr-backed.onrender.com";
+
+async function loadPatients() {
+  const res = await fetch(`${API_URL}/patients`);
+  const patients = await res.json();
+  console.log(patients);
+}
 class ComprehensiveEHRApp {
     constructor() {
         this.currentPatientId = null;
